0643218eaacddf8f30fc7e2a9230dda0749ddfec
Zoey Frakes <zfrakes811@gmail.com>
2020-02-24 20:59:19 -0500

    added new files and installs

---
+++ db/schema.sql
@@ -0,0 +1,13 @@
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+	id PRIMARY KEY
+	burger_name ,
+	devored
+);
+
+
---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+INSERT INTO burger (burger_name) VALUES ('chicken burger');
+INSERT INTO burger (burger_name) VALUES ('bacon burger');
+INSERT INTO burger (client_name) VALUES ('veggie burger');
+
