07929f58c959b590bdf6a8316fe5d2df9ca0b86a
Zoey Frakes <zfrakes811@gmail.com>
2020-02-26 22:12:50 -0500

    added background

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require ("express")
 
-var PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 
-var app = express();
+const app = express();
+const path = require ("path")
 
 // Serve static content for the app from the "public" directory in the application directory.
-app.use(express.static("public"));
+app.use(express.static("./public"));
 
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Set Handlebars.
-var exphbs = require("express-handlebars");
+ exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
