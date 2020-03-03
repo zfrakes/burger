3635012f4e05915e9275d566bcd01dd38fd91691
Zoey Frakes <zfrakes811@gmail.com>
2020-03-03 16:21:17 -0500

    JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,16 @@
 const mysql = require("mysql");
-
+let connection;
+if(process.env.JAWSDB_URL){
+  connection =mysql.createConnection(process.env.JAWSDB_URL)
+} else {
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
-  password: "",
+  password: "zrf081193",
   database: "burgers_db"
 });
-
+}
 // Make connection.
 connection.connect(function(err) {
   if (err) {
